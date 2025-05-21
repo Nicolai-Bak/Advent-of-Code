5bf15515a4d204e6880e8f24a22091596d0d4ded
Nicolai-Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2025-05-21 12:53:43 +1000

    2024

---
+++ 2024/Helpers/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Helpers;
+
+public static class FileOperations
+{
+    public static IEnumerable<string> ReadLines(string path = "data.txt") => File.ReadLines(path);
+}
---
+++ 2024/hello/Program.cs
@@ -0,0 +1,11 @@
+﻿using Helpers;
+
+Console.WriteLine("Hello, World!");
+
+
+var firstLine = FileOperations.ReadLines()
+.FirstOrDefault();
+
+Console.WriteLine(firstLine);
+
+
